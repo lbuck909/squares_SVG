331e4f428491811ae244d2a233d2f1e0e57b7182
LaiSha Buck <laishabuck@gmail.com>
2023-04-29 17:46:15 -0400

    array of end user questions added

--- index.js
+++ index.js
@@ -1,4 +1,41 @@
 const fs = require('inquirer');
 const inquirer = require('fs');
-const 
+const { Triangle, Circle, Square } = require('./lib/shapes');
+
+//end user questions to create logo
+function userPrompt() {
+  .prompt([
+    {
+      type: 'list',
+      name:'What shape would you like for your logo?',
+      choices: [ 'Triangle', 'Square', 'Circle'],
+      response: 'shape',
+      
+    },
+
+    {
+      type: 'input',
+      name:'What text would you like for the logo? (Enter up to three charaters)',
+      response: 'text',
+      
+    },
+
+    {
+      type: 'input',
+      name: 'What color would you like the text? (Enter color keyword or hexadecimal number)',
+      response: ' characterColor',
+      
+    },
+
+    {
+      type: 'input',
+      name: 'What color would you like the chosen shape? (Enter color keyword or hexadecimal number)',
+      response: ' shapeColor',
+      
+    },
+  ])
+  .then((answers) => {
+    if (ans)
+  }
+}
 
